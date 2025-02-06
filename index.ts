23f7d3856eb14e9ce8af47a7d31de54b242b20f9
spectralo <arthur@spectralo.me>
2025-02-06 18:24:48 +0100

    better now!!!

--- index.ts
+++ index.ts
@@ -1,40 +1,43 @@
 const { App } = require("@slack/bolt");
-const axios = require("axios");
+require("better-logging")(console);
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
 
-const file_path = process.env.FILE_PATH;
+const file_path: string = process.env.FILE_PATH || "tokens.json";
 
 (async () => {
   await app.start(process.env.PORT || 3000);
-  console.log("Juice Status is running!");
+  console.info("Started Juice Status Setter !");
   setInterval(async () => {
     const file = Bun.file(file_path);
     const jsonData = await file.json();
+    let counter = 0;
 
     Object.keys(jsonData).forEach((key) => {
-      axios
-        .get("https://juice.hackclub.com/api/user", {
-          headers: {
-            Authorization: `Bearer ${jsonData[key]}`,
-          },
-        })
-        .then((res) => {
-          console.log(res.data.userData.totalJuiceHours);
+      counter++;
+      console.info("");
+      fetch("https://juice.hackclub.com/api/user", {
+        headers: {
+          Authorization: `Bearer ${jsonData[key]}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.info(
+            "Fetched 1 user with : " + data.userData.totalJuiceHours + "h",
+          );
           app.client.users.profile.set({
             token: key,
             profile: {
-              status_text: `${Math.round(res.data.userData.totalJuiceHours * 10) / 10} hours spent juicing!`,
+              status_text: `${Math.round(data.userData.totalJuiceHours * 10) / 10} hours spent juicing!`,
               status_emoji: ":juice:",
             },
           });
-        })
-        .catch((error) => {
-          console.error(error);
         });
     });
-  }, 3000);
+    console.info(`Updated ${counter} users`);
+  }, 10000);
 })();
